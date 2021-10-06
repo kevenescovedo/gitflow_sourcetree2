94eae36c4d3f4bde3610fecaaa14cfaef6eaeaf1
Keven Escovedo <kevenescovedo1916@gmail.com>
2021-10-06 12:42:48 -0300

    title modificado em login

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action='process_login.php' method='POST'>
+        <label>Nome:</lbabel>
+        <input type= 'text' name= 'login' id='login'/> </br>
+        <label>Senha:</lbabel>
+        <input type= 'password' name= 'senha' id='senha'/></br>
+</br>
+        <input type='submit' value='entrar'/> 
+
+    </form>
+</body>
+</html>
