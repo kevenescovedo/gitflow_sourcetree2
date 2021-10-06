53f5364cf86f84390cf48b38ba59dc51e779101a
Keven Escovedo <kevenescovedo1916@gmail.com>
2021-10-06 12:47:23 -0300

    adicionar a tag p com o texto realize login para entrar no sistema

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <form action='process_login.php' method='POST'>
+        <label>Nome:</lbabel>
+        <input type= 'text' name= 'login' id='login'/> </br>
+        <label>Senha:</lbabel>
+        <input type= 'password' name= 'senha' id='senha'/></br>
+</br>
+        <input type='submit' value='entrar'/> 
+
+    </form>
+   <p>realize login para entrar no sistema</p>
+</body>
+</html>
